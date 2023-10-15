cee88a381d323109a71a53d06901d1230dcad39c
Adidem23 <adityasuryawanshi5451@gmail.com>
2023-10-15 18:58:06 +0530

    ClientfileisUpdated

--- src/java_video_stream/JavaClient.java
+++ src/java_video_stream/JavaClient.java
@@ -25,6 +25,11 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -32,47 +37,54 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import java.io.IOException;
+import java.net.*;
 
-/**
- *
- * @author imran
- */
+
 public class JavaClient {
     public static DatagramSocket ds;
     public static DatagramSocket dsaud;
 
     public static void main(String[] args) throws Exception {
+        String receiverIP = "192.168.1.103";
         ds = new DatagramSocket();
         dsaud = new DatagramSocket();
-        
+
         byte[] init = new byte[62000];
         init = "givedata".getBytes();
 
-        byte[] initaud = new byte[62000];
-        initaud = "givedataaud".getBytes();
-        
-        InetAddress addr = InetAddress.getByName("192.168.1.107");
-        
+        //InetAddress addr = InetAddress.getLocalHost();
+        InetAddress addr = InetAddress.getByName(receiverIP);
+
         DatagramPacket dp = new DatagramPacket(init,init.length,addr,4321);
-        
+        DatagramPacket dpaud = new DatagramPacket(init,init.length,addr,54321);
+        System.out.println("Packet Send");
+
         ds.send(dp);
+        dsaud.send(dpaud);
 
-        DatagramPacket dpaud = new DatagramPacket(initaud,initaud.length,addr,54321);
-
-        ds.send(dpaud);
-        
         DatagramPacket rcv = new DatagramPacket(init, init.length);
-        
+        DatagramPacket rcvaud = new DatagramPacket(init, init.length);
+
         ds.receive(rcv);
+        System.out.println("Packet Received");
+        dsaud.receive(rcvaud);
         System.out.println(new String(rcv.getData()));
-        
+
         System.out.println(ds.getPort());
+
+        System.out.println(new String(rcvaud.getData()));
+
+        System.out.println(dsaud.getPort());
         Vidshow vd = new Vidshow();
+        Audshow ad = new Audshow();
+        ad.start();
         vd.start();
-        
+        //ad.start();
+
         String modifiedSentence;
 
-        InetAddress inetAddress = InetAddress.getByName("");
+        InetAddress inetAddress = InetAddress.getByName(receiverIP);
         //.getByName(String hostname); "CL11"
         System.out.println(inetAddress);
 
@@ -100,14 +112,14 @@ class Vidshow extends Thread {
     public static JPanel half = new JPanel(new GridLayout(3,1));
     JLabel jl = new JLabel();
     public static JTextArea ta,tb;
-    
+
     byte[] rcvbyte = new byte[62000];
-    
+
     DatagramPacket dp = new DatagramPacket(rcvbyte, rcvbyte.length);
     BufferedImage bf;
     ImageIcon imc;
-    
-    
+
+
     public Vidshow() throws Exception {
         //sc = mysoc;
         //sc.setTcpNoDelay(true);
@@ -120,21 +132,21 @@ class Vidshow extends Thread {
         jp.add(jl);
         jp.add(half);
         jf.add(jp);
-        
-        
+
+
         JScrollPane jpane = new JScrollPane();
         jpane.setSize(300, 200);
         ta = new JTextArea();
         tb = new JTextArea();
-        
+
         jpane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         jpane.add(ta);
         jpane.setViewportView(ta);
         half.add(jpane);
         half.add(tb);
         ta.setText("Begins\n");
-        
-        
+
+
     }
 
     @Override
@@ -145,11 +157,11 @@ class Vidshow extends Thread {
             do {
                 System.out.println("doing");
                 System.out.println(JavaClient.ds.getPort());
-                
+
                 JavaClient.ds.receive(dp);
                 System.out.println("received");
                 ByteArrayInputStream bais = new ByteArrayInputStream(rcvbyte);
-                
+
                 bf = ImageIO.read(bais);
 
                 if (bf != null) {
@@ -162,12 +174,54 @@ class Vidshow extends Thread {
                 }
                 jf.revalidate();
                 jf.repaint();
-                
+
 
             } while (true);
 
         } catch (Exception e) {
             System.out.println("couldnt do it");
+        }
+    }
+}
+
+class Audshow extends Thread {
+
+
+    byte[] rcvbyte = new byte[62000];
+
+    DatagramPacket dpaud = new DatagramPacket(rcvbyte, rcvbyte.length);
+
+
+    public Audshow() throws Exception {
+        //sc = mysoc;
+        //sc.setTcpNoDelay(true);
+
+
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            AudioFormat format = new AudioFormat(44100, 16, 1, true, false);
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+
+            // socket = new DatagramSocket(PORT);
+
+            System.out.println("Client is receiving audio...");
+            while (true) {
+                byte[] data = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(data, data.length);
+                JavaClient.dsaud.receive(packet);
+                //JavaClient.receive(packet);
+                line.write(data, 0, data.length);
+            }
+        } catch (LineUnavailableException | IOException e) {
+            e.printStackTrace();
         }
     }
 }
@@ -189,20 +243,20 @@ class CThread extends Thread {
             Vidshow.half.add(sender);
             sender.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sentence = Vidshow.tb.getText();
-                Vidshow.ta.append("From myself: "+sentence+"\n");
-                try{
-                outToServer.writeBytes(sentence + '\n');
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    sentence = Vidshow.tb.getText();
+                    Vidshow.ta.append("From myself: "+sentence+"\n");
+                    try{
+                        outToServer.writeBytes(sentence + '\n');
+                    }
+                    catch(Exception E)
+                    {
+
+                    }
+                    Vidshow.tb.setText(null);
                 }
-                catch(Exception E)
-                {
-                
-                }
-                Vidshow.tb.setText(null);
-            }
-        });
+            });
         }
         start();
     }
@@ -214,7 +268,7 @@ class CThread extends Thread {
                 if (RW_Flag == 0) {
                     if(sentence.length()>0)
                     {
-                        
+
                         Vidshow.ta.append(sentence+"\n");
                         Vidshow.ta.setCaretPosition(Vidshow.ta.getDocument().getLength());
                         Vidshow.half.revalidate();
@@ -227,23 +281,22 @@ class CThread extends Thread {
                     }
                 } else if (RW_Flag == 1) {
                     mysent = inFromServer.readLine();
-                    
+
                     Vidshow.ta.append(mysent+"\n");
                     Vidshow.ta.setCaretPosition(Vidshow.ta.getDocument().getLength());
                     Vidshow.half.revalidate();
                     Vidshow.half.repaint();
                     Vidshow.jp.revalidate();
                     Vidshow.jp.repaint();
-                    
-                    
-                    
+
+
+
                     System.out.println("From : " + sentence);
                     sentence = null;
-                    
+
                 }
             }
         } catch (Exception e) {
         }
     }
 }
-
